dfb5d780566fac58589b74008157def9bd7df955
GHenckell <greytho2805@gmail.com>
2022-08-31 14:58:13 +0300

    nav responsive, burger icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,87 +1,125 @@
+import { ReactNode } from "react";
+
 import {
   Stack,
   Flex,
   Box,
   Link,
   Heading,
-  Spacer,
   useColorModeValue,
+  useDisclosure,
+  HStack,
 } from "@chakra-ui/react";
 import React from "react";
 import { IconButton } from "@chakra-ui/button";
+import { HamburgerIcon, CloseIcon, AddIcon } from "@chakra-ui/icons";
 import { FaInstagram, FaFacebook, FaYoutube } from "react-icons/fa";
+
+interface LinkProps {
+  name: string;
+  path: string;
+}
+
+const Links: Array<LinkProps> = [
+  { name: "News", path: "#news" },
+  { name: "Schedule&Prices", path: "/schedule" },
+  { name: "About Us", path: "/about" },
+];
+
+const NavLink = ({ children, path }: { children: ReactNode; path: string }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    href={path}
+  >
+    {children}
+  </Link>
+);
 
 function Header() {
   const linkColor = useColorModeValue("gray.600", "gray.200");
   const linkHoverColor = useColorModeValue("gray.800", "white");
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
-      <Flex align="center" fontSize={{ base: 20, md: 24, sm: 12 }}>
-        <Heading ml="4" size="auto" fontWeight="semibold" color="cyan.400">
-          LatinoMix
-        </Heading>
-        <Stack spacing={4} direction={"row"}>
-          <Link
-            color={linkColor}
-            ml={6}
-            p={2}
-            _hover={{
-              textDecoration: "none",
-              color: linkHoverColor,
-            }}
-            href="#news"
-          >
-            News
-          </Link>
-          <Link
-            color={linkColor}
-            p={2}
-            href="/timetable"
-            _hover={{
-              textDecoration: "none",
-              color: linkHoverColor,
-            }}
-          >
-            Schedule{"&"}Prices
-          </Link>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>
+              <Heading
+                ml="4"
+                size="auto"
+                fontWeight="semibold"
+                color="cyan.400"
+              >
+                LatinoMix
+              </Heading>
+            </Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link.name} path={link.path}>
+                  {link.name}
+                </NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <Link href="https://www.instagram.com/latinomixtanssi/" isExternal>
+              <IconButton
+                aria-label="instagram"
+                icon={<FaInstagram />}
+              ></IconButton>
+            </Link>
+            <Link
+              href="https://www.facebook.com/groups/1106771543020827"
+              isExternal
+            >
+              <IconButton
+                aria-label="facebook"
+                ml={2}
+                icon={<FaFacebook />}
+              ></IconButton>
+            </Link>
+            <Link href="https://www.youtube.com/channel/UCdvuh0Dm3VGkN8TqX94JD-w">
+              <IconButton
+                aria-label="youtube"
+                ml={2}
+                icon={<FaYoutube />}
+              ></IconButton>
+            </Link>
+          </Flex>
+        </Flex>
 
-          <Link
-            color={linkColor}
-            p={2}
-            href="/#about"
-            _hover={{
-              textDecoration: "none",
-              color: linkHoverColor,
-            }}
-          >
-            About Us
-          </Link>
-        </Stack>
-        <Spacer></Spacer>
-        <Link href="https://www.instagram.com/latinomixtanssi/" isExternal>
-          <IconButton
-            aria-label="instagram"
-            icon={<FaInstagram />}
-          ></IconButton>
-        </Link>
-        <Link
-          href="https://www.facebook.com/groups/1106771543020827"
-          isExternal
-        >
-          <IconButton
-            aria-label="facebook"
-            ml={2}
-            icon={<FaFacebook />}
-          ></IconButton>
-        </Link>
-        <Link href="https://www.youtube.com/channel/UCdvuh0Dm3VGkN8TqX94JD-w">
-          <IconButton
-            aria-label="youtube"
-            ml={2}
-            icon={<FaYoutube />}
-          ></IconButton>
-        </Link>
-      </Flex>
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link.name} path={link.path}>
+                  {link.name}
+                </NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
     </>
   );
 }
