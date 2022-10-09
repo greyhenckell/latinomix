3da84096b83bd84c20a9ba5f6379ff3849a4c727
GHenckell <greytho2805@gmail.com>
2022-10-09 21:18:29 +0300

    GA added test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,24 +5,44 @@ import { AppProps } from "next/app";
 
 import Footer from "components/Footer";
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Script from "next/script";
+import * as gtag from "../lib/gtag";
 
 function App({ Component, pageProps }: AppProps): ReactElement {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        strategy="lazyOnload"
+        strategy="afterInteractive"
       />
-      <Script id="google-analytics" strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-        `}
-      </Script>
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
 
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,11 @@
-import {
-  Container,
-  Flex,
-  Box,
-  StackDivider,
-  useColorModeValue,
-  VStack,
-} from "@chakra-ui/react";
+import { Container, Flex, Box, StackDivider, VStack } from "@chakra-ui/react";
 import About from "components/About";
-import Footer from "components/Footer";
+
 import Header from "components/Header";
 import HomeBanner from "components/HomeBanner";
 
-import News from "components/News";
-
 import Head from "next/head";
 import React from "react";
-
-import Script from "next/script";
 
 const Home = () => {
   return (
@@ -38,20 +27,6 @@ const Home = () => {
         />
         <meta property="og:url" content="https://latinomixtanssi.com/" />
         <meta property="og:type" content="website" />
-
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-        `}
-        </Script>
       </Head>
 
       <VStack
