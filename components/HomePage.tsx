c352eacf98c3b2da1cbb6f0f00c02dd4e23d97a1
GHenckell <greytho2805@gmail.com>
2023-08-17 12:19:16 +0300

    bg new photo

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -20,11 +20,11 @@ export default function WithBackgroundImage({ journals }: Props) {
   return (
     <Flex
       id="bgphoto"
-      w={"full"}
+      w={"100%"}
       h={"85vh"}
       backgroundImage={
         //"url(https://images.unsplash.com/photo-1600267175161-cfaa711b4a81?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)"
-        "linear-gradient(to top,rgba(255, 255, 255, 0.155),rgba(0, 0, 0, 0.25)), url(imgs/bgimage.jpeg)"
+        "linear-gradient(to top,rgba(255, 255, 255, 0.155),rgba(0, 0, 0, 0.25)), url(imgs/bg2.jpeg)"
       }
       backgroundSize={"cover"}
       backgroundPosition={"center center"}
